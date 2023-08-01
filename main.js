89291e4eea793cf7651f1f77179f121a2168ff00
Leonardo Christ <christ.leonardo21@gmail.com>
2023-08-01 11:37:28 -0300

    exercicio_pluguins_jquery

---
+++ main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('#cep').mask('00000-000',{
+        placeholder:'99999-999'
+    })
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder:'(99) 99999-9999'
+    })
+    $('#cpf').mask('000.000.000-00',{
+        placeholder:'999.999.999-99'
+    })
+    $('#form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            
+        },
+        submitHandler: function(validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+            console.log('carregou')
+        }
+    })
+    console.log('carregou2')
+})
